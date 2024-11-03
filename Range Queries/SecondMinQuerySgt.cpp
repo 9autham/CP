965d08a374847a54ee15858e14d2ecbeec4b84c2
Gautham <gautam9.2016@gmail.com>
2024-11-03 17:06:37 +0530

    SecondMinimum Query

---
+++ Range Queries/SecondMinQuerySgt.cpp
@@ -0,0 +1,96 @@
+/*
+Problem Statement : You have 2 operations to be done 
+                        1. Update at element at pos to val
+                        2. Query the second - min from l to r
+
+In this Sgt required storing of min and second min
+
+Three things to keep in mind
+    1. Data - What to store in a segment tree nodes
+    2. Merge - How to evaluate the answer using left and right 
+    3. Default - What to return from a node so that answer is ignored (Here returing 1e9 in query function)
+*/
+#include <algorithm>
+#include <bits/stdc++.h>
+#define INF 1e9
+using namespace std;
+vector<pair<int,int>>sgt;
+vector<int>arr;
+pair<int,int> mergeNodes(pair<int,int> left, pair<int,int> right){
+    // {firstMinimun, secondMinimum}
+    if(left.first>right.first){
+        return {right.first, min(right.second,left.first)};
+    }
+    else{
+        return {left.first, min(left.second,right.first)};
+    }
+}
+void build(int index, int start,int end) {
+    if(start == end) {
+        // leaf node
+        sgt[index]={arr[start],INF};
+        return;
+    }
+    int mid = (start + end)/2;
+    build(2*index, start, mid);
+    build(2*index+1, mid+1, end);
+    sgt[index]=mergeNodes(sgt[2*index],sgt[2*index+1]);
+}
+void update(int index, int start, int end, int pos, int val){
+    if(start==end){
+        arr[pos]=val;
+        sgt[index]={arr[pos],INF}; // Root node will not have second Minimum
+        return;
+    }
+    int mid=(start+end)/2;
+    if(pos<=mid){
+        update(2*index,start,mid,pos,val);
+    }
+    else{
+        update(2*index+1, mid+1, end, pos, val);
+    }
+    sgt[index]=mergeNodes(sgt[2*index],sgt[2*index+1]);
+}
+pair<int,int> query(int index, int start, int end, int l, int r){
+    if(start>=l && end<=r){
+        // all elements of l and r are in the range // Full Intersection
+        return sgt[index];
+    }
+    else if(start>r || end<l){
+        // Disjoint - No intersection
+        return {INF,INF}; // Default value
+    }
+    else{
+        int mid = (start+end)/2;
+        pair<int,int> leftAns=query(2*index, start, mid,l,r);
+        pair<int,int> rightAns=query(2*index+1, mid+1, end, l, r);
+        return mergeNodes(leftAns,rightAns);
+    }
+}
+int32_t main() {
+    int n;
+    cin>>n;
+    arr.resize(n);
+    for(auto &i : arr)cin>>i;
+    sgt.resize(4*n);
+    build(1,0,n-1);
+    int q;
+    cin>>q;
+    while(q--){
+        int type;
+        cin>>type;
+        if(type==1){
+            // Update at position-pos and val-x 0 indexed
+            int pos,x;
+            cin>>pos>>x;
+            update(1,0,n-1,pos,x);
+        }
+        else{
+            // query 0 indexed
+            int l,r;
+            cin>>l>>r;
+            cout<<query(1,0,n-1,l,r).second<<endl;
+        }
+    }
+    return 0;
+}
